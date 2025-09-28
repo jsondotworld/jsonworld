cb374c05e8170df319169aadbcb35f38bf98c77f
Hristiyan Dodov <h.dodov@gmail.com>
2025-09-28 15:49:51 +0200

    Add ISO 3166-1 TypeScript types

---
+++ iso-3166-1/types.d.ts
@@ -0,0 +1,6 @@
+import module from './data.ts'
+
+export type IsoCountry = (typeof module)['data'][number]
+export type IsoCountryAlpha2 = IsoCountry['alpha2']
+export type IsoCountryAlpha3 = IsoCountry['alpha3']
+export type IsoCountryNumeric = IsoCountry['numeric']
